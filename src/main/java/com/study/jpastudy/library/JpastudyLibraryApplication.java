7a76072b8964a1866ebb7efb9bb4a3611d968992
jhsseonn <hs125bt@gmail.com>
2024-01-12 17:17:27 +0900

    chore: 프로젝트 기초 세팅

---
+++ src/main/java/com/study/jpastudy/library/JpastudyLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.study.jpastudy.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastudyLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpastudyLibraryApplication.class, args);
+    }
+
+}
